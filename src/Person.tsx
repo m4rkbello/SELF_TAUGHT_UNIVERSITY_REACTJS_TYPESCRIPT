c7ac458220edf2261c9c7bb605e24000999fe80d
m4rkbello <markbello10041999@gmail.com>
2025-11-05 21:43:57 +0800

    ADD USESTATE WITH RENDERING DISPLAY  CONDITION

--- src/Person.tsx
+++ src/Person.tsx
@@ -1,3 +1,39 @@
-const Person = () => {
-    return <div></div>;    
+import { useState } from "react";
+
+interface Person {
+    name: string,
+    age: number,
+    isMarried: boolean,
 }
+
+const Person = (props: Person) => {
+
+    //useState 
+    const [isShowInfo, setShowInfo] = useState<boolean>(false);
+
+    const toggleInfo =()=>{
+        setShowInfo((prev) => !prev);
+    };
+
+    return (
+        <div>
+            {isShowInfo && (
+                <>
+                    <p>
+                        Name: {props.name}
+                        <br />
+                        Age: {props.age}
+                        <br />
+                        This person is {props.isMarried ? "MINYO" : "LAON"} 
+                    </p>
+                </>
+                )     
+            }
+
+            <button onClick={toggleInfo} >Toggle Info</button>
+        </div>    
+    );
+};
+
+export default Person;
+
